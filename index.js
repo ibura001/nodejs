aa108097c072c7767b183fee9efd1a8df5de8b0c
ivannabura <ibura001@gmail.com>
2019-12-12 17:24:05 +0200

    11

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
